e8942195b754db748e14924f174394710451ed26
jonfu86 <jonfu86@gmail.com>
2020-10-14 13:25:46 -0700

    updated with some graphics

--- main.js
+++ main.js
@@ -1,25 +1,86 @@
 const container = document.getElementById('container');
-const box1 = document.getElementById('box1');
-const text1 = document.getElementById('text1');
+const boxes = Array.from(document.getElementsByClassName('box'));
+const Ps = Array.from(document.getElementsByTagName('p'));
 
 const handleClick = (e) => {
-  // console.log(e.target);
-  console.log('container clicked');
+  alert('container clicked');
   e.target.classList.toggle('red');
-
 }
 
-const handleBox1Click = (e) => {
+
+const handleBoxClick = (e) => {
   e.target.classList.toggle('pink');
-  console.log('box 1 clicked');
+  alert('box clicked');
+
 }
 
-const handleText1Click = (e) => {
+const handlePClick = (e) => {
   e.target.classList.toggle('orange');
-  console.log('text 1 clicked');
+  alert(`${e.target.innerHTML} clicked`);
   // e.stopPropagation();
 }
 
 container.addEventListener("click", handleClick);
-box1.addEventListener('click', handleBox1Click);
-text1.addEventListener('click', handleText1Click);
+boxes.forEach(box => {box.addEventListener('click', handleBoxClick)});
+Ps.forEach(p => {p.addEventListener('click', handlePClick)});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const handleClick = (e) => {
+//   console.log(e.target);
+//   const target = e.target;
+//   if (target.tagName === 'P'){
+//     target.classList.toggle('orange');
+//     alert(`${target.innerHTML} clicked`);
+//   }
+
+//   if (target.tagName === 'DIV'){
+//     if(target.classList.contains('box')){
+//       target.classList.toggle('pink');
+//       alert('box clicked');
+//     }
+
+//     if(target.classList.contains('container')) {
+//       alert('container clicked');
+//       target.classList.toggle('red');
+//     }
+//   }
+// }
