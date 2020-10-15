6f4109130ffdc180b9a6ba9905cd45dad4da78f8
jonfu86 <jonfu86@gmail.com>
2020-10-14 18:47:29 -0700

    added some comments

--- main.js
+++ main.js
@@ -7,6 +7,28 @@ const handleClick = (e) => {
   e.target.classList.toggle('red');
 }
 
+//WITH EVENT DELEGATION
+
+// const handleClick = (e) => {
+//   console.log(e.target);
+//   const target = e.target;
+//   if (target.tagName === 'P'){
+//     target.classList.toggle('orange');
+//     alert(`${target.innerHTML} clicked`);
+//   }
+
+//   if (target.tagName === 'DIV'){
+//     if(target.classList.contains('box')){
+//       target.classList.toggle('pink');
+//       alert('box clicked');
+//     }
+
+//     if(target.classList.contains('container')) {
+//       alert('container clicked');
+//       target.classList.toggle('red');
+//     }
+//   }
+// }
 
 const handleBoxClick = (e) => {
   e.target.classList.toggle('pink');
@@ -17,22 +39,14 @@ const handleBoxClick = (e) => {
 const handlePClick = (e) => {
   e.target.classList.toggle('orange');
   alert(`${e.target.innerHTML} clicked`);
-  // e.stopPropagation();
+  e.stopPropagation();
+  e.preventDefault();
 }
 
 container.addEventListener("click", handleClick);
+//CAN COMMENT OUT BELOW CODE WHEN USING EVENT DELEGATION
 boxes.forEach(box => {box.addEventListener('click', handleBoxClick)});
 Ps.forEach(p => {p.addEventListener('click', handlePClick)});
-
-
-
-
-
-
-
-
-
-
 
 
 
